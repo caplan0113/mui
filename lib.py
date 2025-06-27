ad9ab5fc5197ecbd96c00362cf17fd0ca474aabb
caplan0113 <caplan0113-prog@yahoo.co.jp>
2025-06-27 19:03:54 +0900

    update

--- lib.py
+++ lib.py
@@ -12,7 +12,7 @@ import pandas as pd
 warnings.filterwarnings("error", category=UserWarning)
 warnings.filterwarnings("error", category=RuntimeWarning)
 
-N = 22 + 1
+N = 23 + 1
 TASK_ORDER = [
     [4, 3, 1, 2], [3, 1, 4, 2], [2, 4, 1, 3], [2, 4, 3, 1], [3, 4, 2, 1], 
     [4, 1, 3, 2], [1, 4, 2, 3], [4, 3, 2, 1], [1, 3, 4, 2], [4, 2, 1, 3],
